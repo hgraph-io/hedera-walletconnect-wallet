f9bdcd7b4d2c0d8efcc4197adcef38f054d2836c
Fenton Taylor <fentontaylor@gmail.com>
2023-08-08 15:46:43 -0600

    Catch up to changes in web-examples submission

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 require('dotenv').config({ path: '.env.local' })
 
 module.exports = {
-  env: {
-    HEDERA_PRIVATE_KEY: process.env.HEDERA_PRIVATE_KEY
-  },
   reactStrictMode: true,
   webpack(config) {
     config.resolve.fallback = {
--- src/utils/HederaWalletUtil.ts
+++ src/utils/HederaWalletUtil.ts
@@ -4,20 +4,35 @@ export let hederaAddresses: string[]
 export let hederaWallet: HederaWallet
 
 export async function createOrRestoreHederaWallet() {
-  const accountId = process.env.NEXT_PUBLIC_HEDERA_ACCOUNT_ID
-  const privateKey = process.env.HEDERA_PRIVATE_KEY
-  if (!accountId || !privateKey) {
-    throw new Error(
-      'Missing required env vars: `NEXT_PUBLIC_HEDERA_ACCOUNT_ID` and/or `HEDERA_PRIVATE_KEY`'
-    )
-  }
+  try {
+    /**
+     * !!!WARNING!!!
+     * Do not use this approach of storing/accessing private keys in production.
+     * This app is not configured with a secure storage mechanism, so this is just the
+     * best way to make it work and showcase Hedera integration with WalletConnect.
+     */
+    const privateKey = process.env.NEXT_PUBLIC_HEDERA_PRIVATE_KEY
+    const accountAddress = process.env.NEXT_PUBLIC_HEDERA_ACCOUNT_ID
 
-  hederaWallet = new HederaWallet({ accountId, privateKey })
-  const account = await hederaWallet.getAccount()
-  hederaAddresses = [account.accountId]
+    if (!accountAddress || !privateKey) {
+      throw new Error(
+        'Missing required env vars: `NEXT_PUBLIC_HEDERA_ACCOUNT_ID` and/or `HEDERA_PRIVATE_KEY`'
+      )
+    }
 
-  return {
-    hederaWallet,
-    hederaAddresses
+    const accountId = Number(accountAddress.split('.').pop())
+    hederaWallet = HederaWallet.init({ accountId, privateKey })
+    hederaAddresses = [hederaWallet.getAccountAddress()]
+
+    return {
+      hederaWallet,
+      hederaAddresses
+    }
+  } catch (e) {
+    console.error('Failed to initialize Hedera wallet', e)
+    return {
+      hederaWallet: null,
+      hederaAddresses: []
+    }
   }
 }
